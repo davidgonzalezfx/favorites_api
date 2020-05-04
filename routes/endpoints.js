25e5b67684e40bd61bb1cf66bc39db5a49b6b18b
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-03 23:06:50 -0500

    Feature - Endpoints updated

--- routes/endpoints.js
+++ routes/endpoints.js
@@ -7,5 +7,18 @@ router.get('/', function (req, res) {
     message: 'List of favorite websites',
   });
 });
+// Import website controller
+var websiteController = require('../controllers/favoritesController');
+// api endpoints
+router
+  .route('/websites')
+  .get(websiteController.index)
+  .post(websiteController.new);
+router
+  .route('/website/:website_id')
+  .get(websiteController.view)
+  .patch(websiteController.update)
+  .put(websiteController.update)
+  .delete(websiteController.delete);
 // Export API routes
 module.exports = router;
