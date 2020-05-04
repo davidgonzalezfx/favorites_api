fe3ba2c7bdfc3a11122f50ab518769fe8f933d2a
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-03 22:50:53 -0500

    Feature - Setting mongoose and body-parser

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 // Import express
 const express = require('express');
+// Import Body parser
+let bodyParser = require('body-parser');
+// Import Mongoose
+let mongoose = require('mongoose');
 // Initialize express
 const app = express();
-// Send message for default URL
-app.get('/', (req, res) => res.send('Hello World with Express'));
 // Import routes
 let apiRoutes = require('./routes/endpoints.js');
+// Configure bodyparser to handle post requests
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+// Connect to Mongoose and set connection variable
+mongoose.connect(
+  'mongodb://dvdgonzalez:mlab123@ds143461.mlab.com:43461/favorites',
+  { useNewUrlParser: true }
+);
+var db = mongoose.connection;
+// Added check for DB connection
+if (!db) console.log('Error connecting db');
+else console.log('Db connected successfully');
+// Send message for default URL
+app.get('/', (req, res) => res.send('Hello World with Express'));
 // Use Api routes in the App
 app.use('/api', apiRoutes);
 // Launch app to listen to specified port
