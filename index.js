bf9406161c5027be39fdd546dfc7945284459b96
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-03 19:37:05 -0500

    Feature - nodejs + express server v0.1

---
+++ index.js
@@ -0,0 +1,12 @@
+// Import express
+const express = require('express');
+// Initialize express
+const app = express();
+// Send message for default URL
+app.get('/', (req, res) => res.send('Hello World with Express'));
+// Use Api routes in the App
+app.use('/api', apiRoutes);
+// Launch app to listen to specified port
+app.listen(3000, function () {
+  console.log('Running favorites_API on port 3000');
+});
